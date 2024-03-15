7d555f70013e226883d398bf4854f317bc4c74f0
ORANGETAN <113481800+ORANGETAN3422@users.noreply.github.com>
2024-03-15 22:03:11 +1100

    Add files via upload

---
+++ Content/Items/ExperimentalMelee/ReplenishedKnife.cs
@@ -0,0 +1,34 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace OrangesProgression.Content.Items.ExperimentalMelee
+{
+    public class ReplenishedKnife : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 40;
+            Item.width = 44;
+            Item.damage = 25;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTime = 7;
+            Item.useAnimation = 7;
+            Item.DamageType = DamageClass.Melee;
+            Item.crit = 1;
+            Item.autoReuse = true;
+            Item.knockBack = 5;
+            Item.ChangePlayerDirectionOnShoot = true;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ModContent.ItemType<RustedKnife>(), 1);
+            recipe.AddIngredient(ItemID.ArmorPolish, 1);
+            recipe.AddIngredient(ItemID.SilverBar, 10);
+            recipe.AddTile(TileID.Anvils);
+            recipe.Register();
+        }
+    }
+}
