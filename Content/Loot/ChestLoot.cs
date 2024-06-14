5ebd6515a4163bddd1d9a40830d279c454b91f88
PALMFOREST\realp <realpalmforest@gmail.com>
2024-06-14 20:18:55 +1000

    Added "Shooting Star" sky yoyo and made it spawn in sky chests.

--- Content/Loot/ChestLoot.cs
+++ Content/Loot/ChestLoot.cs
@@ -2,11 +2,64 @@ using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using System.Linq;
+using OrangesProgression.Content.Items.Weapons.Melee.Yoyos;
 
 namespace OrangesProgression.Content.Items
 {
     public class ChestLoot : ModSystem
     {
-        
+        public override void PostWorldGen()
+        {
+            // Loop through all chests in the world
+            for (int chestIndex = 0; chestIndex < 1000; chestIndex++)
+            {
+                Chest chest = Main.chest[chestIndex];
+
+                if (chest == null)
+                    continue;
+
+                // If it's a container (chest)
+                if (Main.tile[chest.x, chest.y].TileType == TileID.Containers)
+                {
+                    // If this is a sky chest
+                    if (Main.tile[chest.x, chest.y].TileFrameX == 13 * 36)
+                    {
+                        if(Main.rand.NextBool(4))
+                        {
+                            // Replace primary item with yoyo
+                            chest.item[0].SetDefaults(ModContent.ItemType<ShootingStarYoyo>());
+                        }
+
+                        /*
+                        // Go through each chest slot
+                        for (int slot = 0; slot < 40; slot++)
+                        {
+                            // Get first empty slot
+                            if (chest.item[slot].type == ItemID.None)
+                            {
+                                chest.item[0].SetDefaults(ModContent.ItemType<ShootingStarYoyo>());
+                                break;
+                            }
+                        }
+                        */
+                    }
+                }
+            }
+        }
+
+        private bool ChestContainsItemId(Chest chest, int id)
+        {
+            if(chest != null)
+            {
+                for (int slot = 0; slot < 40; slot++)
+                {
+                    if (chest.item[slot].type == id)
+                        return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
---
+++ Content/Projectiles/Items/Weapons/Melee/ShootingStarYoyoProjectile.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Xna.Framework;
+using Mono.Cecil;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace OrangesProgression.Content.Projectiles.Items.Weapons.Melee
+{
+    public class ShootingStarYoyoProjectile : ModProjectile
+    {
+        int hit = 0;
+
+        public override void SetStaticDefaults()
+        {
+            // The following sets are only applicable to yoyo that use aiStyle 99.
+
+            // YoyosLifeTimeMultiplier is how long in seconds the yoyo will stay out before automatically returning to the player. 
+            // Vanilla values range from 3f (Wood) to 16f (Chik), and defaults to -1f. Leaving as -1 will make the time infinite.
+            ProjectileID.Sets.YoyosLifeTimeMultiplier[Projectile.type] = 5f;
+
+            // YoyosMaximumRange is the maximum distance the yoyo sleep away from the player. 
+            // Vanilla values range from 130f (Wood) to 400f (Terrarian), and defaults to 200f.
+            ProjectileID.Sets.YoyosMaximumRange[Projectile.type] = 160f;
+
+            // YoyosTopSpeed is top speed of the yoyo Projectile.
+            // Vanilla values range from 9f (Wood) to 17.5f (Terrarian), and defaults to 10f.
+            ProjectileID.Sets.YoyosTopSpeed[Projectile.type] = 9.5f;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.timeLeft = default;
+
+
+            Projectile.width = 16; // The width of the projectile's hitbox.
+            Projectile.height = 16; // The height of the projectile's hitbox.
+
+            Projectile.aiStyle = ProjAIStyleID.Yoyo; // The projectile's ai style. Yoyos use aiStyle 99 (ProjAIStyleID.Yoyo). A lot of yoyo code checks for this aiStyle to work properly.
+
+            Projectile.friendly = true; // Player shot projectile. Does damage to enemies but not to friendly Town NPCs.
+            Projectile.DamageType = DamageClass.MeleeNoSpeed; // Benefits from melee bonuses. MeleeNoSpeed means the item will not scale with attack speed.
+            Projectile.penetrate = -1; // All vanilla yoyos have infinite penetration. The number of enemies the yoyo can hit before being pulled back in is based on YoyosLifeTimeMultiplier.
+                                       // Projectile.scale = 1f; // The scale of the projectile. Most yoyos are 1f, but a few are larger. The Kraken is the largest at 1.2f
+        }
+
+        // notes for aiStyle 99: 
+        // localAI[0] is used for timing up to YoyosLifeTimeMultiplier
+        // localAI[1] can be used freely by specific types
+        // ai[0] and ai[1] usually point towards the x and y world coordinate hover point
+        // ai[0] is -1f once YoyosLifeTimeMultiplier is reached, when the player is stoned/frozen, when the yoyo is too far away, or the player is no longer clicking the shoot button.
+        // ai[0] being negative makes the yoyo move back towards the player
+        // Any AI method can be used for dust, spawning projectiles, etc specific to your yoyo.
+
+        public override void PostAI()
+        {
+            if (Main.rand.NextBool(5))
+            {
+                Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.TreasureSparkle); // Makes the projectile emit dust.
+            }
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            this.hit++;
+
+            if(this.hit % 3 == 0 && Projectile.owner == Main.myPlayer)
+            {
+                Vector2 position = new Vector2(target.Center.X - Main.rand.NextFloat(351), target.Center.Y - 700f);
+                Vector2 heading = target.Center - position;
+
+                if (heading.Y < 0f)
+                {
+                    heading.Y *= -1f;
+                }
+
+                if (heading.Y < 20f)
+                {
+                    heading.Y = 20f;
+                }
+
+
+                heading.Normalize();
+                heading *= Projectile.velocity.Length();
+
+                //heading.Y += Main.rand.Next(-40, 41) * 0.02f;
+
+                Projectile.NewProjectile(Projectile.GetSource_FromThis(), position, heading, ProjectileID.Starfury, 5, 1, Main.myPlayer, 0f, 0f);
+            }
+
+            base.OnHitNPC(target, hit, damageDone);
+        }
+    }
+}
